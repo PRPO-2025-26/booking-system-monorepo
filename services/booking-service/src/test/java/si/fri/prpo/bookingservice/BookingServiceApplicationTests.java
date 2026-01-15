324083c3fdc0172cc5e7bc30e169f9086af724bf
ut4228 <ut4228@student.uni-lj.si>
2026-01-15 12:41:24 +0100

    CI: make SpringBootTest DB-independent

--- services/booking-service/src/test/java/si/fri/prpo/bookingservice/BookingServiceApplicationTests.java
+++ services/booking-service/src/test/java/si/fri/prpo/bookingservice/BookingServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package si.fri.prpo.bookingservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookingServiceApplicationTests {
 
 	@Test
